ddfd61e315768447a8712330e5c8c64c6388ce66
Sangsriti <sangsriti14.slg@gmail.com>
2024-10-31 12:43:39 +0530

    added e2e config file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,8 +7,7 @@ import userRoutes from './routes/users';
 import authRoutes from './routes/auth';
 import cookieParser from "cookie-parser";
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
-        .then(()=>console.log("connected to MongoDB"))
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 const app=express();
 app.use(cookieParser());
