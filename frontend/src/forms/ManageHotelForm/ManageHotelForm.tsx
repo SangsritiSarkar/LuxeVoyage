464ee438b72eccfbef2c975ee1956735cf967fb5
Sangsriti <sangsriti14.slg@gmail.com>
2024-11-06 00:23:29 +0530

    added hotel details section of form

---
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -0,0 +1,29 @@
+import { FormProvider, useForm } from "react-hook-form";
+import DetailsSection from "./DetailsSection";
+
+export type HotelFormData = {
+    name: string;
+    city: string;
+    country: string;
+    description: string;
+    type: string;
+    pricePerNight: number;
+    starRating: number;
+    facilities: string[];
+    imageFiles: FileList;
+    adultCount: number;
+    childCount: number; 
+}
+
+const ManageHotelForm = ()=>{
+    const formMethods = useForm<HotelFormData>(); //getting stuff out of useform and sending it to formMethods
+    return ( //spreading all these properties of formMethods to formProvider (just like react context api)
+        <FormProvider {...formMethods}> 
+            <form>
+                <DetailsSection />
+            </form>
+        </FormProvider> //all these individual child sections inside of this form..it can use formMethods from useForm hook
+    );
+};
+
+export default ManageHotelForm;
