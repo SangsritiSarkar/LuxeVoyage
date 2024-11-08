f302fee9acbfc21db68cb99f8c91090eb103175d
Sangsriti <sangsriti14.slg@gmail.com>
2024-11-08 13:02:16 +0530

    added type section in hotel form

--- frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -1,5 +1,6 @@
 import { FormProvider, useForm } from "react-hook-form";
 import DetailsSection from "./DetailsSection";
+import TypeSection from "./TypeSection";
 
 export type HotelFormData = {
     name: string;
@@ -19,8 +20,9 @@ const ManageHotelForm = ()=>{
     const formMethods = useForm<HotelFormData>(); //getting stuff out of useform and sending it to formMethods
     return ( //spreading all these properties of formMethods to formProvider (just like react context api)
         <FormProvider {...formMethods}> 
-            <form>
+            <form className="flex flex-col gap-10">
                 <DetailsSection />
+                <TypeSection />
             </form>
         </FormProvider> //all these individual child sections inside of this form..it can use formMethods from useForm hook
     );
