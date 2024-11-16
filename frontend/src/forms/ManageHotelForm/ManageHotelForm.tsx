ab7fb7885151403e06ccf4eeec03c19d35d7780b
Sangsriti <sangsriti14.slg@gmail.com>
2024-11-16 23:02:58 +0530

    added fetch request for add hotel feature

--- frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -19,16 +19,57 @@ export type HotelFormData = {
     childCount: number; 
 }
 
-const ManageHotelForm = ()=>{
+type Props = {
+    onSave: (hotelFormData: FormData)=>void
+    isLoading: boolean
+}
+
+const ManageHotelForm = ({onSave, isLoading}: Props)=>{
     const formMethods = useForm<HotelFormData>(); //getting stuff out of useform and sending it to formMethods
+    const { handleSubmit } =formMethods;
+
+    const onSubmit = handleSubmit((formDataJson: HotelFormData)=>{
+        //create new FormData object & call our API
+        const formData = new FormData();
+        formData.append("name", formDataJson.name)
+        formData.append("city", formDataJson.city)
+        formData.append("country", formDataJson.country)
+        formData.append("description", formDataJson.description)
+        formData.append("type", formDataJson.type)
+        formData.append("pricePerNight", formDataJson.pricePerNight.toString())
+        formData.append("starRating", formDataJson.starRating.toString())
+        formData.append("adultCount", formDataJson.adultCount.toString())
+        formData.append("childCount", formDataJson.childCount.toString())
+
+        formDataJson.facilities.forEach((facility, index)=>{
+            formData.append(`facilities[${index}]`, facility)
+        });
+
+        //converting imageList i.e. a fileLsit to an array
+        Array.from(formDataJson.imageFiles).forEach((imageFile)=>{
+            formData.append(`imageFiles`, imageFile);
+        });
+
+        onSave(formData);
+    });
+
     return ( //spreading all these properties of formMethods to formProvider (just like react context api)
         <FormProvider {...formMethods}> 
-            <form className="flex flex-col gap-10">
+            <form className="flex flex-col gap-10" onSubmit={onSubmit}>
                 <DetailsSection />
                 <TypeSection />
                 <FacilitiesSection />
                 <GuestsSection />
                 <ImagesSection />
+                <span className="flex justify-end"> 
+                    <button 
+                        disabled={isLoading}
+                        type="submit"
+                        className="bg-pink-700 text-white p-2 font-bold hover:bg-pink-500 text-xl disabled:bg-gray-500"
+                    >
+                        {isLoading? "Saving...": "Save"}
+                    </button>
+                </span>
             </form>
         </FormProvider> //all these individual child sections inside of this form..it can use formMethods from useForm hook
     );
--- frontend/src/pages/AddHotel.tsx
+++ frontend/src/pages/AddHotel.tsx
@@ -1,7 +1,29 @@
+import { useMutation } from "react-query";
 import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
+import { useAppContext } from "../contexts/AppContext";
+import * as apiClient from "../api-client";
 
 const AddHotel = () => {
-    return (<ManageHotelForm/>)
+    const {showToast} = useAppContext();
+    
+    //link fetch req to useMutation hook
+    const { mutate, isLoading } = useMutation(apiClient.addMyHotel, {
+        onSuccess: ()=>{
+            showToast({message: "Hotel Saved!" , type: "SUCCESS"});
+        },
+        onError: ()=>{
+            showToast({message: "Error saving Hotel", type: "ERROR"});
+        },
+    });
+
+
+    //this func calls mutate func
+    const handleSave = (hotelFormData: FormData)=>{
+        mutate(hotelFormData)
+    }
+
+    return <ManageHotelForm onSave={handleSave} isLoading={isLoading}/>;
+    //isLoading ... disables the save button when it is loading
 };
 
 export default AddHotel;
