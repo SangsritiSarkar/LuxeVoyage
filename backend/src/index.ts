7cf850e67ab9b6609f12d518086ecb61c05f0770
Sangsriti <sangsriti14.slg@gmail.com>
2024-11-21 14:39:43 +0530

    modified index file in backend for render to access add hotel page

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL || "https://luxevoyage-zkjx.onrender.com",
     credentials: true,
 }))
 
