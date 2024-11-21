eb1e7016c4b81c9ebb2da5177cdbb80d4b92aa56
Sangsriti <sangsriti14.slg@gmail.com>
2024-11-21 21:26:07 +0530

    modified index

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,7 +23,8 @@ app.use(cookieParser());
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    //origin: process.env.FRONTEND_URL,
+    origin: ["https://luxevoyage-zkjx.onrender.com/"],
     methods: ["POST", "GET"],
     credentials: true,
 }))
